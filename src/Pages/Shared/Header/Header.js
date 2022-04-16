8307fa47c80a517601c029aa66626e2ceee2cf2e
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-04-17 01:34:29 +0600

    Login state done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth)
+
+    const handleLogOut = () => {
+        signOut(auth)
+    }
+
     return (
         <div >
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
@@ -19,7 +28,11 @@ const Header = () => {
                             <Nav.Link as={Link} to='/'>Home</Nav.Link>
                             <Nav.Link as={Link} to='/about'>About</Nav.Link>
                             <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
+                            { 
+                            user ? <button className="btn btn-link text-decoration-none text-primary"onClick={handleLogOut}>LogOut</button>
+                            :
                             <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                            }
                             <Nav.Link as={Link} to='/signup'>SignUp</Nav.Link>
                             <Nav.Link href="#memes">
                             </Nav.Link>
