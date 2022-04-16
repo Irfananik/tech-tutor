7e6c1c7503e3ae7932343f94b9506ae966195f24
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-04-17 01:25:07 +0600

    Login with email and password done

--- src/Pages/Form/Login.js
+++ src/Pages/Form/Login.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const navigate = useNavigate()
@@ -8,18 +10,41 @@ const Login = () => {
     const navigateToSignUp = () => {
         navigate('/signup')
     }
+
+    const emailRef = useRef('')
+    const passwordRef = useRef('')
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth)
+
+    const handleLogin = (event) => {
+        event.preventDefault()
+        const email = emailRef.current.value
+        const password = passwordRef.current.value
+
+        signInWithEmailAndPassword(email, password)
+    }
+
+    if(user){
+        navigate('/')
+    }
+
     return (
         <div className="container my-5">
-            <h3 style={{color: '#566FAA', fontWeight:'bold'}} className="text-center">Login with your email</h3>
-            <Form className="container w-50 mt-4">
+            <h3 style={{ color: '#566FAA', fontWeight: 'bold' }} className="text-center">Login with your email</h3>
+            <Form onSubmit={handleLogin} className="container w-50 mt-4">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                 </Form.Group>
                 <div className="d-flex aling-items-center justify-content-center">
-                    <Button style={{  }} variant="primary" type="submit">
+                    <Button style={{}} variant="primary" type="submit">
                         Login
                     </Button>
                 </div>
