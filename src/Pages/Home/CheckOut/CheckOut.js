f46fde0eb131ed57f06df5f4ed16e7a16ec2b899
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-04-18 13:04:36 +0600

    Checkout section modified

--- src/Pages/Home/CheckOut/CheckOut.js
+++ src/Pages/Home/CheckOut/CheckOut.js
@@ -7,7 +7,7 @@ const CheckOut = () => {
         <div className="my-5">
             <h2 style={{ color: '#566FAA', fontWeight:'bold'}}>Welcome to our course number: {checkoutId}</h2>
             <div>
-                
+                <h4 className="my-4" style={{ color: '#A2B5DC' }}>"This section under maintainence. Please stay with me for updates"</h4>
             </div>
         </div>
     );
